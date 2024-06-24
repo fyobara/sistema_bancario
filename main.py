579d4295244707995d8c0cb6c8d2fac7fe80fc8e
Fernando <fernando.y.obara@gmail.com>
2024-06-23 22:42:34 -0300

    Organização das Funções

--- main.py
+++ main.py
@@ -1,58 +1,73 @@
-saldo = 0
-saques_realizados = 0
-operacao = []
-extrato = []
+def fun_deposito(deposito, saldo):
+    if deposito <= 0.0:
+        print("Digite uma Valor Válido")
+    else:
+        saldo = saldo + deposito
+        print (f"Seu novo saldo e: {saldo}")
+    return deposito, saldo
 
-while 1 == 1:
 
-    print("""Bem-Vindo ao Banco
-      
-      Depósito (1) || Saque (2) || Extrato (3) || Sair (4)
-      """)
+def fun_saque(saque, saldo, saques_realizados):
+    if saque > saldo:
+        print ("Saldo Insuficiente")
+    elif saque > 500.00:
+        print ("Limite de R$500.00")
+    elif saques_realizados >= 3:
+        print ("Limite de 3 Saques")
+    else:
+        saques_realizados+=1
+        saldo = saldo - saque
+        print (f"Seu novo saldo e: {saldo}")
+    return saque, saldo, saques_realizados
 
-    opcao = int(input("Escolha uma Opção"))
-    
 
-    if opcao == 1:
-        print ("Quanto deseja depositar?")
-        deposito = float(input())
-        if deposito < 0:
-            print("Digite uma Valor Válido")
-        else:
-            saldo = saldo + deposito
+def fun_extrato(saldo, operacao, extrato):
+    print(f"Seu Saldo e: {saldo}")
+    for i in range (len (extrato)):
+        print(operacao[i], end="")
+        print(extrato[i])
+
+
+def main():
+
+    saldo = 0
+    saques_realizados = 0
+    operacao = []
+    extrato = []
+
+    while True:
+
+
+        print("""
+              
+                                        Bem-Vindo ao Banco
+     
+        Depósito (1) || Saque (2) || Extrato (3) || Cadastro (4) || Conta (5) || Sair (6)
+        """)
+
+
+        opcao = int(input("Escolha uma Opção "))
+
+        if opcao == 1:
+            print ("Quanto deseja depositar?")
+            deposito = float(input())
+            deposito, saldo = fun_deposito(deposito, saldo)
             operacao.append ("Depósito de: R$")
-            print (f"Seu novo saldo e: {saldo}")
             extrato.append(deposito)
 
+        elif opcao == 2:
+            print ("Quanto deseja sacar?")
+            saque = float(input())
+            saque, saldo, saques_realizados = fun_saque(saque, saldo, saques_realizados)
+            operacao.append ("Saque de: R$")
+            extrato.append(saque) 
 
-    elif opcao == 2:
-        qtd_saques = 0
-        print ("Quanto deseja sacar?")
-        saque = float(input())
-        if saque > saldo:
-            print ("Saldo Insuficiente")
-        elif saque > 500.00:
-            print ("Valor Inválido")
-        elif saques_realizados >= 3:
-            print ("Muitos Saques por Hoje")
+        elif opcao == 3:
+            fun_extrato(saldo, operacao, extrato)
+
+        elif opcao == 4:
+            break
         else:
-                saques_realizados+=1
-                saldo = saldo - saque
-                extrato.append(saque)
-                operacao.append ("Saque de: R$")
-                print (f"Seu novo saldo e: {saldo}")
-            
-        
-    elif opcao == 3:
-        print(f"Seu Saldo e: {saldo}")
+            print("Escolha uma Opção Válida")
 
-        for i in range (len (extrato)):
-            print(operacao[i], end="")
-            print(extrato[i])
-
-
-    elif opcao == 4:
-        break
-
-    else:
-        print("Escolha uma Opção Válida")
+main()
